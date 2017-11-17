c783a8b6566f36ffaccd792bf8504fd9b1c7026c
ThornWu <wzzz00118@gmail.com>
2017-11-17 16:12:01 +0800

    response tencent

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,99 @@
+var http = require('http');
+var util = require('util');
 var express = require('express');
+var fs=require('fs');
+var querystring=require('querystring');
 var router = express.Router();
+var session = require('express-session');
+var filestore=require('session-file-store')(session);
+var weixin=require('weixin-api');
+var bodyParser=require('body-parser');
+//session
+router.use(session({
+  secret: 'BrokenArrow',
+  resave: false,
+  saveUninitialized:false,
+  store:new filestore(),
+  cookie:{
+    maxAge:10*1000
+  }
+}));
 
+router.use(function (req, res, next) {
+  if (!req.session.user) {
+    req.session.user = null;
+  } 
+  next();
+})
+weixin.token='xiaogu';
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if(req.query.signature!=undefined){
+    if (weixin.checkSignature(req)) {
+      res.send(200, req.query.echostr);
+    } else {
+      res.send(200, 'fail');
+    }
+  }
+  else{
+    res.render('index', { title: 'Express',user:req.session.user});    
+  }
+});
+router.get('/login',function(req,res){
+  var openid=req.query.openid;
+  var openExist=false;
+  if(req.session.user!=null){
+    res.redirect('/');
+  }
+  else{
+    fs.readFile('./data/openid.db',function(err,data){
+      if(err){
+        throw err;
+      }
+      var jsonObj=JSON.parse(data);
+      var openList=jsonObj["open"];
+      for(var obj in openList){
+        if(openList[obj].openid==openid){
+          req.session.user=openList[obj].username;
+          openExist=true;
+          break;
+        }
+      }
+      if(openExist){
+        res.redirect('/');
+      }
+      else{
+        res.render('login',{ title: 'Express'});    
+      }
+    });
+  }
+});
+
+router.post('/checkLogin',function(req,res){
+  var uname=req.body['username'];
+  var pwd=req.body['password'];
+  fs.readFile('./data/user.db',function(err,data){
+    var status=400;    
+    if(err){
+      throw err;
+    }
+    var jsonObj=JSON.parse(data);
+    var userList=jsonObj["user"];
+    for(var obj in userList){
+      if(userList[obj].username==uname && userList[obj].password==pwd){
+        status=200;
+        req.session.user=uname;
+        break;
+      }
+    }
+    res.status(status);
+    res.end();
+  });
+});
+
+router.get('/logout',function(req,res){
+  req.session.user=null;
+  res.redirect('/');
 });
 
 module.exports = router;
