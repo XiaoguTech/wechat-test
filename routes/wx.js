92bd010e8b6a99fe6f4938ab8541eaac655bfec4
ThornWu <wzzz00118@gmail.com>
2017-11-21 13:04:09 +0800

    test-open-id-3

--- routes/wx.js
+++ routes/wx.js
@@ -76,8 +76,11 @@ router.post('/',async function(req,res,next){
                 '<CreateTime>'+createTime+'</CreateTime>'+
                 '<MsgType><![CDATA[text]]></MsgType>'+
                 '<Content><![CDATA[您好，请绑定您的Openid账号：]]></Content>'+
+                '<MsgType><![CDATA[link]]></MsgType>'+
+                '<Title><![CDATA[点击绑定]]></Title>'
                 '<Url><![CDATA[http://wechat.xiaogu-tech.com/login?openid='+midObject.json.xml.FromUserName+']]></Url>'+
                 '</xml>';
+                
               }
 
             // handleMessage(midObject.json.xml,res);            
