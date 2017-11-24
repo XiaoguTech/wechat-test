5e08f6db333afd8424447ef1cf466186ee48f080
ThornWu <wzzz00118@gmail.com>
2017-11-24 15:39:48 +0800

    better-alert-api

--- routes/alert.js
+++ routes/alert.js
@@ -19,22 +19,12 @@ router.get('/refresh',function(req,res){
   var db = req.app.db;  
   db.alerts.find({"orgID":orgID},function(err,data){
     if(data.length){
-      var alertObject={};
       var jsonObj=data[0];
       var alertArray=jsonObj["alertArray"];
       var latestTime=alertArray[0].time;
       var latestMessage=alertArray[0].message;
-      for(var obj in alertArray){
-        var time=new Date(alertArray[obj].time);
-        var showTime=time.getFullYear()+"-"+(time.getMonth()+1)+"-"+time.getDate()+" "+time.getHours()+":"+time.getMinutes()+":"+time.getSeconds();
-        alertArray[obj].time=showTime;
-      }
+      var alertObject={alertArray:alertArray,latestTime:latestTime,latestMessage:latestMessage};
     }
-    var formatTime=alertArray[0].time;
-    alertObject.alertArray=alertArray;
-    alertObject.latestTime=latestTime;
-    alertObject.latestMessage=latestMessage;
-    alertObject.formatTime=formatTime;
     res.send(alertObject);
   });    
 
@@ -54,16 +44,77 @@ router.get('/getNewNum',function(req,res){
         orgID:sOrgID
       });
     }else{
+      // found
       var aAlert = result.alertArray;
       var iAlertIndex = aAlert.findIndex(function(element){
         return element.time <= dTimeStamp;
       });
       // not found 
       if(iAlertIndex === -1){
-        return res.status(200).json({message:"not found"});
+        return res.status(200).json({
+          message:"not found"
+        });
       }else{
-      // found
         return res.status(200).json({iNewNum:iAlertIndex});
+      }
+    }
+  });
+});
+
+
+
+/*
+get 方法
+alert/getLatestMessage?timestamp=xxx
+if 新消息的数量==0 返回数量
+else if 新消息的数量>0 返回数量、最新的消息内容和最新的时间戳
+ */
+
+router.get('/getLatestMessage',function(req,res){
+  var dTimeStamp = req.query.timestamp;
+  var sOrgID = req.session.user;
+  var db = req.app.db.alerts;
+  db.findOne({"orgID":sOrgID},function(err,result){
+    if(result == null){
+      // not found orgID
+      return res.status(200).json({
+        message:"not found your orgID",
+        orgID:sOrgID
+      });
+    }else{
+      // found
+      var aAlert = result.alertArray;
+      var iAlertIndex = aAlert.findIndex(function(element){
+        return element.time <= dTimeStamp;
+      });
+      // not found 
+      if(iAlertIndex === -1){
+        if(aAlert.length>0){
+          return res.status(200).json({
+            message:"not found",
+            iNewNum:iAlertIndex,
+            sMessage:aAlert[0].message,
+            sTime:aAlert[0].time
+          });
+        }else{
+          return res.status(200).json({
+            message:"not found",
+            iNewNum:iAlertIndex,
+          });
+        }
+
+      }else{
+        if(iAlertIndex === 0){
+          return res.status(200).json({
+            iNewNum:iAlertIndex
+          });
+        }else{
+          return res.status(200).json({
+            iNewNum:iAlertIndex,
+            sMessage:aAlert[0].message,
+            sTime:aAlert[0].time
+          });
+        }
       }
     }
   });
